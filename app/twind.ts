f4ce5f711e7ece5690c3f30b28e4c0448cf4d662
Resi Respati <resir014@gmail.com>
2022-03-10 15:04:55 +0700

    chore: include tailwind config in setup (#1)

--- app/twind.ts
+++ app/twind.ts
@@ -1,8 +1,9 @@
 import { setup } from '@twind/tailwind';
+import tailwindConfig from '../tailwind.config.js';
 
 // Exporting a function instead of calling setup() directly,
 // to avoid modules with side effects.
 // https://remix.run/docs/en/v1/guides/constraints#no-module-side-effects
 export function setupTwind() {
-  setup();
+  setup(tailwindConfig);
 }
